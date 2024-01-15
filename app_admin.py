c13d5909071d95d5362ef862a80430abc55d8746
Ecrevisse <romeo.incardona@gmail.com>
2024-01-15 17:39:01 +0100

    change admin page dropdown order to oldest first

--- app_admin.py
+++ app_admin.py
@@ -7,14 +7,19 @@ st.set_page_config(page_title="H2Power - Admin Portal")
 
 firebase.initialize_firebase()
 conversations = firebase.get_all_conversations()
+conversations = {
+    k: v
+    for k, v in sorted(conversations.items(), key=lambda item: -item[1]["timestamp"])
+}
 conv_ids = list(conversations.keys()) if conversations else []
 
-conv_names = {}
-for conv_id in conv_ids:
+conv_names = list(range(len(conv_ids)))
+conv_dropdown_ids = [i for i in range(len(conv_ids))]
+for i, conv_id in enumerate(conv_ids):
     conv = conversations[conv_id]
     dt_object = datetime.fromtimestamp(conv["timestamp"])
     conv["timestamp"] = dt_object.strftime("%d/%m/%Y %H:%M:%S")
-    conv_names[conv_id] = f"{conv['timestamp']} - {conv_id}"
+    conv_names[i] = f"{conv['timestamp']}"
 
 # Sidebar for filtering
 # add button to clear cache
@@ -22,13 +27,13 @@ if st.sidebar.button("Refresh"):
     st.cache_data.clear()
     st.rerun()
 selected_conversation = st.sidebar.selectbox(
-    "Select a conversation", conv_ids, format_func=lambda x: conv_names[x]
+    "Select a conversation", conv_dropdown_ids, format_func=lambda x: conv_names[x]
 )
 
 if not conversations:
     st.title("No conversations yet")
 else:
-    conv = conversations[selected_conversation]
+    conv = conversations[conv_ids[selected_conversation]]
 
     st.title(f"Chat History from {conv['timestamp']}")
     if "messages" not in conv:
